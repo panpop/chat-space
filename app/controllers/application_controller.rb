c5cdd04f5475dc1856e8885b3193e47a4eb90c50
panpop <shikibi_s@yahoo.co.jp>
2018-10-03 15:49:35 +0900

    add application_controller.rb
    add
    app/controllers/application_controller.rb
    
    add strong parameter keys:[:name]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
 end
