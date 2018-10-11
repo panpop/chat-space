0046b130403fd6f48f0f52727054e31406abf389
panpop <shikibi_s@yahoo.co.jp>
2018-10-11 11:13:45 +0900

    add before_action GroupsController
    add before_action
    app/controllers/groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -26,8 +27,13 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
