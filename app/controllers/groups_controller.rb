18300bcd40566db7bb6c0a182e249e33e0089e7b
panpop <shikibi_s@yahoo.co.jp>
2018-10-11 11:11:30 +0900

    add method GroupsController
    add method update
    app/controllers/groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,14 @@ class GroupsController < ApplicationController
     end
   end
 
+  def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
