6a569a8162b4719e0a7bbdcb3c72df5e2887ab1b
panpop <shikibi_s@yahoo.co.jp>
2018-10-24 10:15:36 +0900

    Correction#3 JS(message.js)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -41,7 +41,7 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
-      $('form')[0].reset();
+      $('#new_message')[0].reset();
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
